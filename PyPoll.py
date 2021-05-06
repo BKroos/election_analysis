ec0e24ea9eb6cf7d91b33e3058a2581eceeb35ff
BKroos <btkroos@gmail.com>
2021-05-06 18:57:33 -0500

    Add files via upload

--- PyPoll.py
+++ PyPoll.py
@@ -33,36 +33,41 @@ with open(file_to_load) as election_data:
             candidate_options.append(candidate_name)
             candidate_votes[candidate_name] = 0
         candidate_votes[candidate_name] += 1
+with open(file_to_save, "w") as txt_file:
+    election_results = (
+        f"\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-------------------------\n")
+
+    print(election_results, end="")
+    # Save the final vote count to the text file.
+    txt_file.write(election_results)
     for candidate_name in candidate_votes:
-    # Retrieve vote count of a candidate.
+        # Retrieve vote count of a candidate.
         votes = candidate_votes[candidate_name]
-    # Calculate the percentage of votes.
+        # Calculate the percentage of votes.
         vote_percentage = float(votes) / float(total_votes) * 100
 
-        #Print out candidates and votes
-        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
-    
-    # Determine winning candidate and votes
+            #Print out candidates and votes
+        candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        print(candidate_results)
+        txt_file.write(candidate_results)
+
+        # Determine winning candidate and votes
         if (votes > winning_count) and (vote_percentage > winning_percentage):
-            winning_count = votes
-            winning_percentage = vote_percentage
-            winning_candidate = candidate_name
+                winning_count = votes
+                winning_percentage = vote_percentage
+                winning_candidate = candidate_name
     winning_candidate_summary = (
-         f"--------------------------\n"
-         f"Winner: {winning_candidate}\n"
-         f"Winning Vote Count: {winning_count:,}\n"
-         f"Winning Percentage: {winning_percentage:.1f}%\n"
-         f"--------------------------\n")
-         
+            f"--------------------------\n"
+            f"Winner: {winning_candidate}\n"
+            f"Winning Vote Count: {winning_count:,}\n"
+            f"Winning Percentage: {winning_percentage:.1f}%\n"
+            f"--------------------------\n")
+            
     print(winning_candidate_summary)
+    txt_file.write(winning_candidate_summary)
     
-
-
-open(file_to_save, "w")
-# Using the with statement open the file as a text file.
-with open(file_to_save, "w") as txt_file:
-
-    # Write three counties to the file
-    txt_file.write("Arapahoe\nDenver\nJefferson")
 
 
